09812771cd0dc0c07e35a2c9ad8cf40f409d5ad0
valerak2000 <valera_k2000@inbox.ru>
2015-04-07 18:06:41 +0300

    root layout

--- Rp5_fx/src/org/rp5/Rp5.java
+++ Rp5_fx/src/org/rp5/Rp5.java
@@ -1,400 +1,201 @@
 package org.rp5;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.prefs.Preferences;
 
-import org.eclipse.jface.action.CoolBarManager;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.action.Separator;
-import org.eclipse.jface.action.StatusLineManager;
-import org.eclipse.jface.action.ToolBarManager;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.jface.viewers.CellEditor;
-import org.eclipse.jface.viewers.CheckboxCellEditor;
-import org.eclipse.jface.viewers.ColorCellEditor;
-import org.eclipse.jface.viewers.ComboBoxCellEditor;
-import org.eclipse.jface.viewers.TableViewer;
-import org.eclipse.jface.viewers.TextCellEditor;
-import org.eclipse.jface.window.ApplicationWindow;
-import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.*;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.*;
-import org.eclipse.swt.widgets.*;
-import org.rp5.ui.model.DownloadWeatherRp5;
-import org.rp5.ui.model.ParseXml;
-import org.rp5.ui.model.Rp5TableModel;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+
+import org.controlsfx.dialog.Dialogs;
 import org.rp5.ui.model.WeatherRp5;
-import org.rp5.ui.view.AboutAction;
-import org.rp5.ui.view.ExitAction;
-import org.rp5.ui.view.Rp5ContentProvider;
-import org.rp5.ui.view.Rp5LabelProvider;
+import org.rp5.ui.model.WeatherRp5ListWrapper;
+import org.rp5.ui.view.RootLayoutController;
 
-import com.btr.proxy.search.ProxySearch;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
- * @param args 1 - Path to DataBase
- *             2 - Name of DataBase
- *             3 - User Name
- *             4 - User Password
- *             5 - Count of keys
- * @throws SAXException
- * @throws ParserConfigurationException
+ * @param args 
+ * @throws
  */
-public class Rp5 extends ApplicationWindow {
+public class Rp5 extends Application {
 	// A static instance to the running application
-	private static Rp5 APP;
-	private TableViewer tv;
-
-	// The actions
-//	private OpenAction openAction;
-//	private CloseAction closeAction;
-//	private ConfigAction configAction;
-//	private PrintAction printAction;
-//	private AboutAction aboutAction;
-	private ExitAction exitAction;
-
-	private static final String[] columnNames = new String[13];
-	private static HashMap<String, ImageDescriptor> hashImages;
-
-	{
-		APP = this;
-	}
-	/**
-	 * Gets the running application
-	*/
-	public static final Rp5 getApp() {
-	    return APP;
-	}
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+    /**
+     * The data as an observable list of WeatherRp5.
+     */
+    private ObservableList<WeatherRp5> weatherRp5Data = FXCollections.observableArrayList();
 
 	public Rp5() {
-		super(null);
-
-	    hashImages = new HashMap<String, ImageDescriptor>();
-		hashImages.put("open", ImageDescriptor.createFromFile(Rp5.class, "/org/images/fileopen.png"));
-		hashImages.put("close", ImageDescriptor.createFromFile(Rp5.class, "/org/images/fileclose.png"));
-		hashImages.put("print", ImageDescriptor.createFromFile(Rp5.class, "/org/images/fileprint.png"));
-		hashImages.put("configure", ImageDescriptor.createFromFile(Rp5.class, "/org/images/configure.png"));
-		hashImages.put("help", ImageDescriptor.createFromFile(Rp5.class, "/org/images/help.png"));
-		hashImages.put("about", ImageDescriptor.createFromFile(Rp5.class, "/org/images/about.png"));
-		hashImages.put("exit", ImageDescriptor.createFromFile(Rp5.class, "/org/images/exit.png"));
-
-		columnNames[0] = "time_step";
-		columnNames[1] = "datetime";
-		columnNames[2] = "G";
-		columnNames[3] = "HHii";
-		columnNames[4] = "cloud_cover";
-		columnNames[5] = "precipitation";
-		columnNames[6] = "pressure";
-		columnNames[7] = "temperature";
-		columnNames[8] = "humidity";
-		columnNames[9] = "wind_direction";
-		columnNames[10] = "wind_velocity";
-		columnNames[11] = "falls";
-		columnNames[12] = "drops";
-
-	    // Create the actions
-//	    openAction = new OpenAction();
-//	    closeAction = new CloseAction();
-//	    configAction = new ConfigAction();
-//	    printAction = new PrintAction();
-//	    aboutAction = new AboutAction();
-	    exitAction = new ExitAction();
-
-	    addMenuBar();
-	    addCoolBar(SWT.NONE);
-	    addStatusLine();
 	}
 
-	public void run() {
-	    // Don't return from open() until window closes
-		setBlockOnOpen(true);
-	    // Open the main window
-	    open();
-	    // Dispose the display
-	    Display.getCurrent().dispose();
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 
-	protected void configureShell(Shell shell) {
-		super.configureShell(shell);
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("Weather from Rp5");
 
-		// Set the title bar text and the size
-		shell.setText("");
-		//reaction on close main window by "close button" 
-		shell.addShellListener(new ShellAdapter() {
-			public void shellClosed(ShellEvent e) {
-			}
-		});
-	}
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Rp5.png"));
 
-	protected Control createContents(Composite parent) {
-	    Composite composite = new Composite(parent, SWT.NONE);
-	    composite.setLayout(new GridLayout(1, false));
-	    // Add the TableViewer
-	    final TableViewer tv = new TableViewer(composite, SWT.FULL_SELECTION);
-	    tv.setContentProvider(new Rp5ContentProvider());
-	    tv.setLabelProvider(new Rp5LabelProvider());
-//	    tv.setSorter(new ZebraViewerSorter());
-	    // Set up the table
-	    createTable(tv, SWT.SINGLE | SWT.BORDER | SWT.FULL_SELECTION | 
-                		SWT.V_SCROLL | SWT.H_SCROLL);
+        initRootLayout();
+       // showPersonOverview();
+    }
 
-	    //set size of window like table
-	    getShell().setSize(tv.getTable().computeSize(SWT.DEFAULT, SWT.DEFAULT).x, 300);
-
-	    return composite;
-	}
-
-	public ImageDescriptor getImageFor(String cmd) {
-		return (ImageDescriptor) hashImages.get(cmd.toLowerCase());
-	}
-
-	protected Table createTable(TableViewer tv, int mode) {
-		Table table = tv.getTable();
-		table.setLayoutData(new GridData(GridData.FILL_BOTH));
-
-		for (int i = 0; i < columnNames.length; i++) {
-		    createTableColumn(table, SWT.NONE, columnNames[i], 150);
-		}
-
-		addTableContents(new Object[] {new String[] {""}});
-	    table.setHeaderVisible(true);
-	    table.setLinesVisible(true);
-/*
-	    // Create the cell editors
-	    CellEditor[] editors = new CellEditor[4];
-	    editors[0] = new TextCellEditor(table);
-	    editors[1] = new CheckboxCellEditor(table);
-	    editors[2] = new ComboBoxCellEditor(table, AgeRange.INSTANCES, SWT.READ_ONLY);
-	    editors[3] = new ColorCellEditor(table);
-
-	    // Set the editors, cell modifier, and column properties
-	    tv.setColumnProperties(columnNames);
-	    tv.setCellModifier(new PersonCellModifier(tv));
-	    tv.setCellEditors(editors);
-*/
-	    return table;
-	}
-
-
-	protected TableColumn createTableColumn(Table table, int style, String title, int width) {
-	    TableColumn tc = new TableColumn(table, style);
-	    tc.setText(title);
-	    tc.setResizable(true);
-	    tc.setWidth(width);
-	    tc.pack();
-
-	    return tc;
-	}	
-
-	
-	protected void addTableContents(Object[] items) {
-//		tv.getTable().setItemCount(0);
-//		tv.setInput(Rp5TableModel.INSTANCE.getLabels());  
-
-//		for(int i = 0; i < items.length; i++) {
-//	        String[] item = (String[]) items[i];
-//	        TableItem ti = new TableItem(tv.getTable(), SWT.NONE);
-//	        ti.setText(item);
-//	    }
-	}
-
-	
-	/**
-	 * Shows an error
-	 *
-	 * @param msg the error
-	*/
-	public void showError(String msg) {
-	    MessageDialog.openError(getShell(), "Error", msg);
-	}
+    /**
+     * Returns the data as an observable list of Persons. 
+     * @return
+     */
+    public ObservableList<WeatherRp5> getWeatherRp5Data() {
+        return weatherRp5Data;
+    }
 
 	/**
-	 * Creates the menu at the top of the shell where most
-	 * of the programs functionality is accessed.
-	 *
-	 * @return		The <code>Menu</code> widget that was created
+	 * Initializes the root layout and tries to load the last opened city
+	 * 
 	 */
+   	public void initRootLayout() {
+	    try {
+	        // Load root layout from fxml file.
+	        FXMLLoader loader = new FXMLLoader();
+	        loader.setLocation(Rp5.class
+	                .getResource("ui/view/RootLayout.fxml"));
+	        rootLayout = (BorderPane) loader.load();
 
-	protected MenuManager createMenuManager() {
-	    // Create the main menu
-	    MenuManager mmng = new MenuManager();
+	        // Show the scene containing the root layout.
+	        Scene scene = new Scene(rootLayout);
+	        primaryStage.setScene(scene);
 
-	    // Create the File menu
-	    MenuManager fileMenu = new MenuManager("File_menu_title");
-	    mmng.add(fileMenu);
+	        // Give the controller access to the main app.
+	        RootLayoutController controller = loader.getController();
+	        controller.setMainApp(this);
 
-	    // Add the actions to the File menu
-//	    fileMenu.add(openAction);
-//	    fileMenu.add(closeAction);
-//	    fileMenu.add(new Separator());
-//	    fileMenu.add(printAction);
-//	    fileMenu.add(new Separator());
-//	    fileMenu.add(configAction);
-//	    fileMenu.add(new Separator());
-	    fileMenu.add(exitAction);
+	        primaryStage.show();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    }
 
-	    // Create the Help menu
-	    MenuManager helpMenu = new MenuManager("Help");
-	    mmng.add(helpMenu);
+	    // Try to load last opened person file.
+	    File file = getWeatherRp5FilePath();
+	    if (file != null) {
+	        loadWeatherRp5DataFromFile(file);
+	    }
+    }
 
-	    // Add the actions to the Help menu
-//	    helpMenu.add(aboutAction);
+   	/**
+     * Returns the main stage.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
 
-	    return mmng;
-	}
+    /**
+     * Returns the WeatherRp5 file preference, i.e. the file that was last opened.
+     * The preference is read from the OS specific registry. If no such
+     * preference can be found, null is returned.
+     * 
+     * @return
+     */
+    public File getWeatherRp5FilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(Rp5.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
 
-	  /**
-	   * Creates the toolbar for the application
-	   */
+    /**
+     * Sets the file path of the currently loaded file. The path is persisted in
+     * the OS specific registry.
+     * 
+     * @param file the file or null to remove the path
+     */
+    public void setWeatherRp5FilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(Rp5.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
 
-	protected ToolBarManager createToolBarManager(int style) {
-	    // Create the toolbar manager
-	    ToolBarManager tbm = new ToolBarManager(style);
-	    // Add the file actions
-//	    tbm.add(openAction);
-//	    tbm.add(closeAction);
+            // Update the stage title.
+            primaryStage.setTitle("Weather from Rp5 - " + file.getName());
+        } else {
+            prefs.remove("filePath");
 
-//	    tbm.add(new Separator());
-//	    tbm.add(printAction);
+            // Update the stage title.
+            primaryStage.setTitle("Weather from Rp5");
+        }
+    }
 
-//	    tbm.add(new Separator());
-//	    tbm.add(configAction);
+    /**
+     * Loads WeatherRp5 data from the specified file. The current person data will
+     * be replaced.
+     * 
+     * @param file
+     */
+    public void loadWeatherRp5DataFromFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(WeatherRp5ListWrapper.class);
+            Unmarshaller um = context.createUnmarshaller();
 
-	    // Add a separator
-	    tbm.add(new Separator());
+            // Reading XML from the file and unmarshalling.
+            WeatherRp5ListWrapper wrapper = (WeatherRp5ListWrapper) um.unmarshal(file);
 
-	    // Add the about action
-//	    tbm.add(aboutAction);
+            weatherRp5Data.clear();
+            weatherRp5Data.addAll(wrapper.getWeatherRp5());
 
-	    tbm.add(new Separator());
-	    tbm.add(exitAction);
+            // Save the file path to the registry.
+            setWeatherRp5FilePath(file);
 
-	    return tbm;
-	}
+        } catch (Exception e) { // catches ANY exception
+            Dialogs.create()
+                    .title("Error")
+                    .masthead("Could not load data from file:\n" + file.getPath())
+                    .showException(e);
+        }
+    }
 
-	/**
-	* Creates the coolbar for the application
-	*/
+    /**
+     * Saves the current WeatherRp5 data to the specified file.
+     * 
+     * @param file
+     */
+    public void savePersonDataToFile(File file) {
+        try {
+            JAXBContext context = JAXBContext
+                    .newInstance(WeatherRp5ListWrapper.class);
+            Marshaller m = context.createMarshaller();
+            m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 
-	protected CoolBarManager createCoolBarManager(int style) {
-		// Create the coolbar manager
-		CoolBarManager cbm = new CoolBarManager(style);
+            // Wrapping our person data.
+            WeatherRp5ListWrapper wrapper = new WeatherRp5ListWrapper();
+            wrapper.setWeatherRp5(weatherRp5Data);
 
-		// Add the toolbar
-		cbm.add(createToolBarManager(SWT.FLAT));
+            // Marshalling and saving XML to the file.
+            m.marshal(wrapper, file);
 
-		return cbm;
-	}
-
-	/**
-	* Creates the status line manager
-	*/
-
-	protected StatusLineManager createStatusLineManager() {
-		return new StatusLineManager();
-	}
-
-/*	private void createHelpMenu(Menu menuBar) {
-		//Help Menu
-		MenuItem item = new MenuItem(menuBar, SWT.CASCADE);
-		item.setText(confZebra.getString("Help_menu_title"));	
-		Menu menu = new Menu(shell, SWT.DROP_DOWN);
-		item.setMenu(menu);
-		
-		//Help -> About Text Editor
-		MenuItem subItem = new MenuItem(menu, SWT.NONE);
-		subItem.setText(confZebra.getString("About"));
-		subItem.setImage(getImageFor("help"));
-		subItem.addSelectionListener(new SelectionAdapter() {
-			public void widgetSelected(SelectionEvent e) {
-				MessageBox box = new MessageBox(shell, SWT.NONE);
-				box.setText(confZebra.getString("About_1"));
-				box.setMessage(confZebra.getString("About_2") + shell.getText());
-				box.open();
-			}
-		});
-	}*/
-
-/*	*//**
-	 * To compare entries (rows) by the given column
-	 *//*
-	private class RowComparator implements Comparator<String []> {
-		private int column;
-		
-		*//**
-		 * Constructs a RowComparator given the column index
-		 * @param col The index (starting at zero) of the column
-		 *//*
-		public RowComparator(int col) {
-			column = col;
-		}
-		
-		*//**
-		 * Compares two rows (type String[]) using the specified
-		 * column entry.
-		 * @param obj1 First row to compare
-		 * @param obj2 Second row to compare
-		 * @return negative if obj1 less than obj2, positive if
-		 * 			obj1 greater than obj2, and zero if equal.
-		 *//*
-		public int compare(String[] row1, String[] row2) {
-//			String[] row1 = (String[])obj1;
-//			String[] row2 = (String[])obj2;
-			
-			return row1[column].compareTo(row2[column]);
-		}
-	}
-*/
-	/**
-	 * Closes the application
-	*/
-	public boolean close() {
-	    return super.close();
-	}
-
-	/**
-	 * Enables or disables the actions
-	 *
-	 * @param enable true to enable, false to disable
-	*/
-/*
-	private void enableActions(boolean enable) {
-	    newAction.setEnabled(enable);
-	    openAction.setEnabled(enable);
-	    saveAction.setEnabled(enable);
-	    saveAsAction.setEnabled(enable);
-	    addBookAction.setEnabled(enable);
-	    removeBookAction.setEnabled(enable);
-	    showBookCountAction.setEnabled(enable);
-//	    aboutAction.setEnabled(enable);
-//	    exitAction.setEnabled(enable);
-	}
-*/
-	public static void main(String[] args) {
-		Rp5 rp5 = new Rp5();
-    	rp5.run();
-/*
-		ProxySearch proxySearch = ProxySearch.getDefaultProxySearch();
-//    	   	ProxySearch.main(null);
-    	
-       	ProxySelector myProxySelector = proxySearch.getProxySelector();
-    	ProxySelector.setDefault(myProxySelector);
- 
-    	DownloadWeatherRp5 dwRp5 = new DownloadWeatherRp5();
- 
-//    	InputStream ruIs = new FileInputStream("ru.xml");
-
-    	ParseXml px = new ParseXml();
-    	List<WeatherRp5> wrp5 = px.parseWeather(dwRp5.getWeather("4429"));
-        
-    	for(WeatherRp5 iwr5: wrp5) {
-    		iwr5.Print();
-    	}
-//    	px.parseWeather(ruIs);
-        
-
-//    	ruIs.close(); 
-*/
+            // Save the file path to the registry.
+            setWeatherRp5FilePath(file);
+        } catch (Exception e) { // catches ANY exception
+            Dialogs.create().title("Error")
+                    .masthead("Could not save data to file:\n" + file.getPath())
+                    .showException(e);
+        }
     }
 }
--- Rp5_fx/src/org/rp5/ui/model/WeatherRp5.java
+++ Rp5_fx/src/org/rp5/ui/model/WeatherRp5.java
@@ -22,30 +22,91 @@ wind_direction - направление ветра (С-северный, СВ-северо-восточный и т.д.). Нап
 wind_velocity - скорость ветра
 falls - тип осадков (0 - отсутствие осадков, 1 - дождь, 2 - дождь со снегом, 3 - снег)
 drops - коэффициент количества снежинок или капель дождя для визуализации. Возможные значения: 0.5, 1, 2, 3, 4, 5, 6, 7, 8. Чем больше это значение, тем количество или размер капель или снежинок должны быть больше.
-*/
+ */
 package org.rp5.ui.model;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
 import java.util.Date;
 
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import javafx.beans.property.StringProperty;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+/**
+ * Model class for a WeatherRp5.
+ *
+ * @author
+ */
 public class WeatherRp5 {
-	int timeStep;
-	Date dateTime;
-	String g;
-	String hhii;
-	int cloudCover;
-	double precipitation;
-	int pressure;
-	int temperature;
-	int humidity;
-	String windDirection;
-	int windVelocity;
-	int falls;
-	int drops;
-	
+	private final IntegerProperty timeStep;
+	private final ObjectProperty<Date> dateTime;
+	private final StringProperty g;
+	private final StringProperty hhii;
+	private final IntegerProperty cloudCover;
+	private final DoubleProperty precipitation;
+	private final IntegerProperty pressure;
+	private final IntegerProperty temperature;
+	private final IntegerProperty humidity;
+	private final StringProperty windDirection;
+	private final IntegerProperty windVelocity;
+	private final IntegerProperty falls;
+	private final IntegerProperty drops;
+
+	/**
+	 * Default constructor.
+	 */
+	public WeatherRp5() {
+		this(0, null, null, null, 0, 0, 0, 0, 0, null, 0, 0, 0);
+	}
+
+	/**
+	 * Constructor with some initial data.
+	 * 
+	 * @param timeStep
+	 * @param dateTime
+	 * @param g
+	 * @param hhii
+	 * @param cloudCover
+	 * @param precipitation
+	 * @param pressure
+	 * @param temperature
+	 * @param humidity
+	 * @param windDirection
+	 * @param windVelocity
+	 * @param falls
+	 * @param drops
+	 */
+	public WeatherRp5(int timeStep, Date dateTime, String g, String hhii,
+			int cloudCover, double precipitation, int pressure,
+			int temperature, int humidity, String windDirection,
+			int windVelocity, int falls, int drops) {
+		this.timeStep = new SimpleIntegerProperty(timeStep);
+		this.dateTime = new SimpleObjectProperty<Date>(dateTime);
+		this.g = new SimpleStringProperty(g);
+		this.hhii = new SimpleStringProperty(hhii);
+		this.cloudCover = new SimpleIntegerProperty(cloudCover);
+		this.precipitation = new SimpleDoubleProperty(precipitation);
+		this.pressure = new SimpleIntegerProperty(pressure);
+		this.temperature = new SimpleIntegerProperty(temperature);
+		this.humidity = new SimpleIntegerProperty(humidity);
+		this.windDirection = new SimpleStringProperty(windDirection);
+		this.windVelocity = new SimpleIntegerProperty(windVelocity);
+		this.falls = new SimpleIntegerProperty(falls);
+		this.drops = new SimpleIntegerProperty(drops);
+	}
+
 	public void Print() {
 		System.out.println("time_step=" + timeStep);
-		System.out.println("datetime=" + new SimpleDateFormat("yyyy-MM-dd hh:mm").format(dateTime));
+		System.out.println("datetime="
+				+ new SimpleDateFormat("yyyy-MM-dd hh:mm").format(dateTime));
 		System.out.println("G=" + g);
 		System.out.println("HHii=" + hhii);
 		System.out.println("cloud_cover=" + cloudCover);
@@ -58,5 +119,161 @@ public class WeatherRp5 {
 		System.out.println("falls=" + falls);
 		System.out.println("drops=" + drops);
 		System.out.println();
+	}
+
+	public final IntegerProperty timeStepProperty() {
+		return this.timeStep;
+	}
+
+	public final int getTimeStep() {
+		return this.timeStepProperty().get();
+	}
+
+	public final void setTimeStep(final int timeStep) {
+		this.timeStepProperty().set(timeStep);
+	}
+
+	public final ObjectProperty<Date> dateTimeProperty() {
+		return this.dateTime;
+	}
+
+	public final Date getDateTime() {
+		return this.dateTimeProperty().get();
+	}
+
+	public final void setDateTime(final Date dateTime) {
+		this.dateTimeProperty().set(dateTime);
+	}
+
+	public final StringProperty gProperty() {
+		return this.g;
+	}
+
+	public final java.lang.String getG() {
+		return this.gProperty().get();
+	}
+
+	public final void setG(final java.lang.String g) {
+		this.gProperty().set(g);
+	}
+
+	public final StringProperty hhiiProperty() {
+		return this.hhii;
+	}
+
+	public final java.lang.String getHhii() {
+		return this.hhiiProperty().get();
+	}
+
+	public final void setHhii(final java.lang.String hhii) {
+		this.hhiiProperty().set(hhii);
+	}
+
+	public final IntegerProperty cloudCoverProperty() {
+		return this.cloudCover;
+	}
+
+	public final int getCloudCover() {
+		return this.cloudCoverProperty().get();
+	}
+
+	public final void setCloudCover(final int cloudCover) {
+		this.cloudCoverProperty().set(cloudCover);
+	}
+
+	public final DoubleProperty precipitationProperty() {
+		return this.precipitation;
+	}
+
+	public final double getPrecipitation() {
+		return this.precipitationProperty().get();
+	}
+
+	public final void setPrecipitation(final double precipitation) {
+		this.precipitationProperty().set(precipitation);
+	}
+
+	public final IntegerProperty pressureProperty() {
+		return this.pressure;
+	}
+
+	public final int getPressure() {
+		return this.pressureProperty().get();
+	}
+
+	public final void setPressure(final int pressure) {
+		this.pressureProperty().set(pressure);
+	}
+
+	public final IntegerProperty temperatureProperty() {
+		return this.temperature;
+	}
+
+	public final int getTemperature() {
+		return this.temperatureProperty().get();
+	}
+
+	public final void setTemperature(final int temperature) {
+		this.temperatureProperty().set(temperature);
+	}
+
+	public final IntegerProperty humidityProperty() {
+		return this.humidity;
+	}
+
+	public final int getHumidity() {
+		return this.humidityProperty().get();
+	}
+
+	public final void setHumidity(final int humidity) {
+		this.humidityProperty().set(humidity);
+	}
+
+	public final StringProperty windDirectionProperty() {
+		return this.windDirection;
+	}
+
+	public final java.lang.String getWindDirection() {
+		return this.windDirectionProperty().get();
+	}
+
+	public final void setWindDirection(final java.lang.String windDirection) {
+		this.windDirectionProperty().set(windDirection);
+	}
+
+	public final IntegerProperty windVelocityProperty() {
+		return this.windVelocity;
+	}
+
+	public final int getWindVelocity() {
+		return this.windVelocityProperty().get();
+	}
+
+	public final void setWindVelocity(final int windVelocity) {
+		this.windVelocityProperty().set(windVelocity);
+	}
+
+	public final IntegerProperty fallsProperty() {
+		return this.falls;
+	}
+
+	public final int getFalls() {
+		return this.fallsProperty().get();
+	}
+
+	public final void setFalls(final int falls) {
+		this.fallsProperty().set(falls);
+	}
+
+	public final IntegerProperty dropsProperty() {
+		return this.drops;
+	}
+
+	public final int getDrops() {
+		return this.dropsProperty().get();
+	}
+
+	public final void setDrops(final int drops) {
+		this.dropsProperty().set(drops);
 	}
 }
